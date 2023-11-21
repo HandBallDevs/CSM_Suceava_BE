b8e3ff1422709829a99c8a9e347946207e221264
Alex101120 <133406167+Alex101120@users.noreply.github.com>
2023-11-21 17:28:11 +0200

    V1

---
+++ CSU_Suceava_BE/CSU_Suceava_BE/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+namespace CSU_Suceava_BE.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+           
+            if (IsValidAdmin(username, password))
+            {
+               
+                var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, username),
+                new Claim(ClaimTypes.Role, "Administrator")
+            };
+
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                var authProperties = new AuthenticationProperties
+                {
+                    IsPersistent = true,
+                    ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(20)
+                };
+
+                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+
+                return RedirectToAction("Index", "AdminPanel"); //Admin Panel
+            }
+
+            
+            return RedirectToAction("Login");
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home"); 
+        }
+
+        private bool IsValidAdmin(string username, string password)
+        {
+            //De conectat la BazaDedatepentru check
+
+            return username == "admin" && password == "parola";
+        }
+    }
+}
--- CSU_Suceava_BE/CSU_Suceava_BE/Program.cs
+++ CSU_Suceava_BE/CSU_Suceava_BE/Program.cs
@@ -1,9 +1,23 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+///////////////////////////////////Login Feature////////////////////////////////////
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+}).AddCookie(options =>
+{
+    options.LoginPath = "/Account/Login"; //Pagina de Login (de completat) Completat Curent cu Views/TestLogin.html
+});
+/////////////////////////////////////////////////////////////////////////////////////
+
+builder.Services.AddAuthorization();
 var app = builder.Build();
 
 app.UseSwagger();
@@ -11,6 +25,7 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
