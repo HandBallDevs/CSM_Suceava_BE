0ff5adbf5673904b53fc2fa09ac344a6917dc10c
tihurjui <tiberiu.hurjui@assist.ro>
2023-11-25 21:21:35 +0200

    CSU-20-Implement-Staff-Controller-and-CRUD-operations

---
+++ CSU_Suceava_BE/CSU_Suceava_BE.Application/Models/StaffDto.cs
@@ -0,0 +1,18 @@
+﻿using CSU_Suceava_BE.Domain.Enums;
+
+namespace CSU_Suceava_BE.Application.Models
+{
+    public class StaffDto
+    {
+        public Guid Id { get; set; }
+        public string Nume { get; set; } = null!;
+        public string Prenume { get; set; } = null!;
+        public string Nationalitate { get; set; } = null!;
+        public TipLot TipLot { get; set; }
+        public string Post { get; set; } = null!;
+        public string URLPoza { get; set; } = null!;
+        public DateTime DataNastere { get; set; }
+        public double Inaltime { get; set; }
+        public string? Descriere { get; set; }
+    }
+}
--- CSU_Suceava_BE/CSU_Suceava_BE/Program.cs
+++ CSU_Suceava_BE/CSU_Suceava_BE/Program.cs
@@ -1,10 +1,19 @@
+using CSU_Suceava_BE.Application.Interfaces;
+using CSU_Suceava_BE.Application.Mapper;
+using CSU_Suceava_BE.Application.Services;
 using CSU_Suceava_BE.Infrastructure.Contexts;
+using CSU_Suceava_BE.Infrastructure.Interfaces;
+using CSU_Suceava_BE.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => options.EnableAnnotations());
+builder.Services.AddScoped<IStaffRepository, StaffRepository>();
+builder.Services.AddScoped<IStaffService, StaffService>();
+
+builder.Services.AddAutoMapper(typeof(StaffProfile));
 
 builder.Services.AddDbContext<EFContext>();
 
